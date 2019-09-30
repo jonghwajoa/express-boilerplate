0ece266e765e3b0eab3eec073c141af57cf883cd
jonghwa <jonghwa0710@naver.com>
2019-09-30 14:40:39 +0900

    chore: 포트 80에서 3000으로 변경

--- src/bin/www.js
+++ src/bin/www.js
@@ -2,7 +2,7 @@ import app from '../app';
 import { dbSync } from '../database/index';
 
 dbSync().then(result => {
-  app.listen(80, () => {
+  app.listen(3000, () => {
     console.log('Server start');
   });
 });
