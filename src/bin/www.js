5ed13c2c37652ff45c355ee46a239ffc429b0c24
jonghwa <jonghwa0710@naver.com>
2019-10-17 10:16:49 +0900

    fix: 포트번호 상수화

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,8 +1,10 @@
 import app from '../app';
 import dbSync from './dbSync';
 
+const { PORT } = process.env;
+
 dbSync({ force: false }).then(() => {
-  app.listen(3000, () => {
+  app.listen(PORT, () => {
     console.log('Server start');
   });
 });
